a3bebe611b9828fafee23ff2a5c7cd4375cac02f
Tools-app <3532340532@qq.com>
2025-07-06 11:16:49 +0800

    feat: 添加下载文件函数
    Signed-off-by: Tools-app <3532340532@qq.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,17 @@
 pub fn option_to_str<T: Default>(option: Option<T>) -> T {
     option.unwrap_or_default()
 }
+
+pub mod http {
+    use std::io::Read;
+
+    use anyhow::Result;
+
+    pub fn download_file(url: &str) -> Result<u8> {
+        let mut res = reqwest::blocking::get(url)?;
+        let mut body = String::new();
+        res.read_to_string(&mut body)?;
+        println!("{}", body);
+        Ok(2)
+    }
+}
